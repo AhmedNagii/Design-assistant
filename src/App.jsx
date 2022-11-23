9e70434977cfb08fe32b30fa3ebb01c3a0f2e20f
Ahmed nagi <36850727+AhmedNagii@users.noreply.github.com>
2022-11-23 19:51:02 +0200

    adding home page features
    getting schema and save to local storage and making the design responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import IconsPage from "./pages/IconsPage";
 
 import "./css/App.css";
 
+const queryClinet = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/searchicons" element={<IconsPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClinet}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/searchicons" element={<IconsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/IconsPage.jsx
+++ src/pages/IconsPage.jsx
@@ -1,9 +1,13 @@
 import Navbar from "../components/Navbar";
-
+import Sidebar from "../components/Sidebar";
+import useToggle from "../hooks/useToggle";
 export default function IconsPage() {
+  const [toggleState, toggle] = useToggle(false);
+
   return (
     <>
-      <Navbar />
+      <Navbar handelClick={toggle} isOpen={toggleState} />
+      <Sidebar isOpen={toggleState} />
       <h1 className="text">Serach Icons</h1>
     </>
   );
